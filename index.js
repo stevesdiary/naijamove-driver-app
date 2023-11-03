ea174dcf027ea6c8423570265223457e3294622e
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2023-11-03 12:08:04 +0100

    Pass function arguments as an object

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-//Use Array.some to check occurrence in array
+//Pass function arguments as an object
 /*
-If we want to check only occurrence means value exist or not then use Array.some instead of Array.find.
-The some() method checks if any array items pass a test implemented by the provided function. If the function returns true, some() returns true and stops.
-The some() method does not change the original array.
+Parameters are part of a function definition. A JavaScript function can have any number of parameters. When we invoke a function and pass some values to that function, these values are called function arguments.
+If a function has more than 1 parameter, it is hard to figure out what these arguments mean when the function is called. When you pass the arguments, the order is important.
+A better way is to create a function with object (with properties) parameters like in the example. When we pass the argument contained in an object it is pretty
 */
-const assets = [
-   {id: 1, title: "V-1", type: "video"},
-   {id: 2, title: "V-2", type: "video"},
-   {id: 3, title: "A-3", type: "audio"}
-];
 
-const hasVideoAsset = assets.some(asset => asset.type === "video");
-console.log(hasVideoAsset); //true
+//code
+const createProduct = (name, price, category, brandId) => {
+   //code to create product
+};
+createProduct("Product-1", 500, 1, 1);
+
+//Better code
+
+const createProduct =({name, price, categoryId, brandId}) => {
+   //code to create product
+};
+createProduct({
+   name: "Product-1",
+   price: 500,
+   categoryId: 1,
+   brandId: 1
+})
