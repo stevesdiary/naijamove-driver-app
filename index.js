a96b6578221b7eeddc6f74ef34d7788fbbfdfc38
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2023-11-03 11:41:05 +0100

    Use Array.some to check occurrence in array

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
-// Avoid delete keyword
-// Avoid a delete keyword to remove a property from an object. This way mutates the original object and hence leads to unpredictable behavior and makes debugging difficult.
-// A better way to delete a property without mutating the original object is by using the rest operator (...). Use the rest operator (...) to create a new copy without the given property name.
-const employee = {
-   id : 1,
-   name: "john",
-   salary: 50000
-};
-
-const { salary, ...newEmployee } = employee;
-console.log(newEmployee); //{ id: 1, name: 'john' }
-
-// Use of falsy bouncer
+//Use Array.some to check occurrence in array
 /*
-A falsy value is a value that is considered false when examined as a Boolean.
-Falsy Bouncer means removing all falsy values from an array.
-Falsy values in JavaScript are false, null, 0, undefined, NaN, and "" (empty string).
-Pass the Boolean to Array.filter as the first argument and it will serve as a falsy bouncer.
+If we want to check only occurrence means value exist or not then use Array.some instead of Array.find.
+The some() method checks if any array items pass a test implemented by the provided function. If the function returns true, some() returns true and stops.
+The some() method does not change the original array.
 */
+const assets = [
+   {id: 1, title: "V-1", type: "video"},
+   {id: 2, title: "V-2", type: "video"},
+   {id: 3, title: "A-3", type: "audio"}
+];
 
-const numbersWithFalsyValues = [7, null, 11, 17, false, NaN];
-
-const numbers = numbersWithFalsyValues.filter(Boolean);
-console.log(numbers); //[ 7, 11, 17 ]
-
-const namesWithFalsyValues = ["Roy", null, "", undefined, "James"]
-const names = namesWithFalsyValues.filter(Boolean);
-console.log(names);
+const hasVideoAsset = assets.some(asset => asset.type === "video");
+console.log(hasVideoAsset); //true
