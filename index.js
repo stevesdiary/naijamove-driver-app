5ae82c68723b4fd8e82b803b2a07871b1efcf2c4
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2023-11-03 12:38:17 +0100

    Skip values in array destructuring

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-//Pass function arguments as an object
-/*
-Parameters are part of a function definition. A JavaScript function can have any number of parameters. When we invoke a function and pass some values to that function, these values are called function arguments.
-If a function has more than 1 parameter, it is hard to figure out what these arguments mean when the function is called. When you pass the arguments, the order is important.
-A better way is to create a function with object (with properties) parameters like in the example. When we pass the argument contained in an object it is pretty
-*/
+// Skip values in array destructuring
 
-//code
-const createProduct = (name, price, category, brandId) => {
-   //code to create product
+/**
+ Destructuring means breaking down a complex structure into simpler parts. 
+ Array destructuring is a way that allows us to extract an array’s value into new variables.
+ Sometimes we don't need some values from the array means we want to skip those values. 
+ During the destructuring arrays, if you want to skip some values, use an empty placeholder comma. 
+ This is a clean way to skip values.
+ */
+const scores = [50, 40, 30, 80, 90, ];
+
+const [, , ...restScores] = scores;
+
+console.log(restScores); //[ 30, 80, 90 ]
+
+// Filter with JSON.stringify
+const employee = {
+   id: 1,
+   name: "James",
+   address: {
+      city: "Lagos",
+      state: "Lagos",
+      country: "Nigeria"
+   }
 };
-createProduct("Product-1", 500, 1, 1);
-
-//Better code
-
-const createProduct =({name, price, categoryId, brandId}) => {
-   //code to create product
-};
-createProduct({
-   name: "Product-1",
-   price: 500,
-   categoryId: 1,
-   brandId: 1
-})
+const filters = ["name", "address", "city", "country"];
+filterEmployee = JSON.stringify(employee, filters);
+console.log(filterEmployee); //{"name":"James","address":{"city":"Lagos","country":"Nigeria"}}
