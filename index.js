d96c66cafb251e76a2b2613e72ce979130a38640
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2023-11-03 10:46:31 +0100

    Add property to object

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-// Check property exists in an object 
-const employee = {
-   id: 1,
-   age: 24,
-   name: 'Joan',
-   salary: 50000
+// Conditionally add property to object using spread operator
+
+const includeSalary = true;
+const employee = { 
+   id: 1, 
+   name: "John",
+   ...(includeSalary && {salary: 50000 })
 };
-
-const isSalaryExist = 'salary' in employee;
-console.log(isSalaryExist); //true
-
-const isAgeExist = 'age' in employee;
-console.log(isAgeExist); //true
-
-const isGenderExist = 'gender' in employee;
-console.log(isGenderExist); //false
+console.table(employee)
