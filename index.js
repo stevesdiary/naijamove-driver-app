a4fb141d024705f9ad464a734200f585492e018e
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2023-11-08 14:45:04 +0100

    Form table with nested for loop

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-// Power of JSON.stringify replacer parameter
-
-/**
- * The JSON.stringify() method converts a JavaScript object to a JSON string.
-The 2nd parameter to JSON.stringify() is a replacer or filter that can be a function or array.
-When 2nd parameter is passed as a replacer function, it alters the behavior of the stringification process. 
-As a function, it takes two parameters, the key and the value being stringified.
- */
-
-
-const employee = {
-   id: 1,
-   name: "Joe",
-   salary: 30_000
-};
-const doubleSalary =  (key, value) => {
-   return key === "salary" ? value * 2 : value;
-};
-
-const result = JSON.stringify(employee, doubleSalary);
-console.log(result); //{"id":1,"name":"Joe","salary":60000}
+//Form table with nested for loop
+let myTable = [];
+for (let i = 0; i < 3; i++){
+   myTable.push([]);
+   for (let j = 0; j < 7; j++){
+      myTable[i].push(j);
+   }
+}
+console.table(myTable)
