39105c98a019abd96f6845678702ad7f3b0613a3
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2023-11-08 15:44:55 +0100

    iterate over the elements of an array using 'for of loop'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
 There is another loop we can use to iterate over the elements of an array:
-the for of loop.
+the "for of loop".
  */
 
 let names = ["Chantal", "John", "Maxime", "Bobbi", "Jair"];
