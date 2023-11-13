93e702ab7795dec495753502d6d2d36dfc958cec
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2023-11-13 13:59:03 +0100

    Callback function

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ const myForEach = (arr, cb) => {
 myForEach(array, (name) => {
    console.log(name);
 })
+
+
+const loadPokemon = (id, cb) => {
+   fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+   .then(res => res.json())
+   .then(data => {
+      cb(data)
+   })
+}
+
+loadPokemon(50, (pokemon)=> {
+   console.log(pokemon)
+})
