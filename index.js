ecf602c323381547c1200b68205cbf0987fb5ca3
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2023-11-18 21:46:58 +0100

    Using switch

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const myAge = 20;
-// Calculating dogYears
-let earlyYears = 2; 
-earlyYears *= 10.5;
-let laterYears = myAge - 2;
-laterYears *= 4;
-console.log(earlyYears, laterYears)
-const myAgeInDogYears = earlyYears + laterYears;
-console.log(myAgeInDogYears);
-//Turn name to lower case
-let myName = "Steve";
-myName = myName.toLowerCase();
-console.log(myName);
+//Using switch 
+let athleteFinalPosition = 'first place';
+switch (athleteFinalPosition) {
+  case 'first place':
+    console.log('You get the gold medal!');
+    break;
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+  case 'third place':
+    console.log('You get the bronze medal!');
+    break;
+  default: 
+    console.log('No medal awarded.');
+    break;
+}
